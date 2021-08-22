7bd3064ecb6a0755fad2c34f41862def6adb600d
DariaTsypkina <77797372+DariaTsypkina@users.noreply.github.com>
2021-08-22 18:10:48 +0300

    added components

---
+++ language_cards/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './assets/components/header';
+import Main from './assets/components/main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;

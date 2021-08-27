6c7b40faae7ce7fd969707b61697820457a5797d
dariatsipkina <88425193+dariatsipkina@users.noreply.github.com>
2021-08-27 20:54:51 +0300

    +

--- language_cards/src/App.js
+++ language_cards/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Header from './assets/components/header';
-import Main from './assets/components/main';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
+      
     </div>
   );
 }

0db24b71103a10bd756682189fa138c8ee0f3e26
DariaTsypkina <77797372+DariaTsypkina@users.noreply.github.com>
2021-08-28 20:27:08 +0300

    changes

--- language_cards/src/App.js
+++ language_cards/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Main from './components/main';
 
 function App() {
   return (
     <div className="App">
-      
+      <Main/>
     </div>
   );
 }
---
+++ language_cards/src/components/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './card';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <Card />
+            </div>
+        )
+    }
+}
+
+export default Main;

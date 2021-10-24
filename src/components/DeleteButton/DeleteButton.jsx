3dea0d1a7dd1aabb03c10cc422aa58a467e38114
DariaTsypkina <77797372+DariaTsypkina@users.noreply.github.com>
2021-10-24 22:43:37 +0300

    fix styles

--- src/components/DeleteButton/DeleteButton.jsx
+++ src/components/DeleteButton/DeleteButton.jsx
@@ -4,6 +4,6 @@ import styles from './button.module.scss';
 export default function CancelChangeButton(props) {
     const { handleDelete } = props;
     return (
-        <button className={styles.button} onClick={handleDelete}>&#128465;</button>
+        <button className={styles.button} onClick={handleDelete}>Del</button>
     );
 }
